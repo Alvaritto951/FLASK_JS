595f5d71023d3f3f42fd72d250763c912359ddf9
Alvaritto951 <alvaritto951@gmail.com>
2022-10-07 00:03:14 +0200

    Hasta aquí hemos llegado

--- registros_ig/routes.py
+++ registros_ig/routes.py
@@ -1,8 +1,10 @@
+from http.client import CREATED
 import sqlite3
-from flask import jsonify, render_template #Función de Flask -- Transforma el argumento que tengas y lo convierte en JSON.
+from http import HTTPStatus
+from flask import jsonify, render_template, request #Función de Flask -- Transforma el argumento que tengas y lo convierte en JSON.
 
 from registros_ig import app
-from registros_ig.models import select_all
+from registros_ig.models import insert, select_all
 
 @app.route("/index")
 def index():
@@ -29,7 +31,22 @@ def all_movements():
 
 @app.route("/api/v1.0/new", methods=["POST"])
 def new():
-    return "Esto hará un alta"
+    registro = request.json
+    #TODO Validar registro
+    try:
+        insert([registro["date"], registro["concept"], registro["quantity"]])
+        return jsonify({
+            "status": "OK"
+        }), HTTPStatus.CREATED #201
+    except sqlite3.Error as e:
+        return jsonify(
+            {
+                "status": "Error",
+                "data": str(e)
+            }
+        ), HTTPStatus.BAD_REQUEST #400
+
+    
 
 @app.route("/api/v1.0/delete/<int:id>", methods=["DELETE"])
 def delete(id):
--- registros_ig/static/js/spa.js
+++ registros_ig/static/js/spa.js
@@ -1,15 +1,15 @@
 peticion_todos = new XMLHttpRequest() //Se utiliza new para crear una nueva variable- XMLHttpRequest es una instancia de un objeto
+peticion_alta = new XMLHttpRequest()
 
 function peticion_todos_handler() {
     if (this.readyState === 4) { //XMLHttprequest --> Petición en estado terminada
         if (this.status === 200) { //this es el manejador (XMLHttpRequest)
-            alert(this.responseText)
-            //TODO: Procesar el responseText, transformarlo en un objeto(diccionario) de javascript
-            //      y transformarlo en filas de la tabla
+            //alert(this.responseText) -- Sirve para que nos salga en el navegador, antes de poder hacer nada
 
+            //Procesar el responseText, transformarlo en un objeto(diccionario) de javascript y transformarlo en filas de la tabla
             const los_datos = JSON.parse(this.responseText) // Los datos pasan a ser una lista de diccionarios
             const la_tabla = document.querySelector("#movements_table") //Crear la estructura de tabla
-            const movimientos = los_datos.data
+            const movimientos = los_datos.data //Viene de routes dentro de la api all, del jsonify (línea 18 de routes.py)
             
             for (let i=0; i < movimientos.length; i++) {
                 item = movimientos[i]
@@ -38,10 +38,68 @@ function peticion_todos_handler() {
     }
 }
 
+function peticion_alta_handler() {
+    if (this.readyState === 4) {
+        if (this.status === 201) {
+            peticion_todos.open("GET", "http://localhost:5000/api/v1.0/all", true)
+            peticion_todos.onload = peticion_todos_handler
+            peticion_todos.onerror = function() {alert("No se ha podido completar la petición de movimientos")}
+            peticion_todos.send()
+        } else {
+            alert("Se ha producido un error en el alta de movimientos")
+        }
+    }
+}
+
+
+function altaMovimiento(ev) {
+    ev.preventDefault()
+    
+    const date = document.querySelector("#date").value //Capturar datos
+    const concept = document.querySelector("#concept").value
+    const quantity = document.querySelector("#quantity").value
+
+    if (concept === "") { //Validar datos capturados
+        alert("Debes informar el concepto")
+        return
+    }
+
+    if (quantity == 0 || quantity === "") {
+        alert("Debes informar una cantidad positiva o negativa")
+        return
+    }
+
+    const hoy = new Date().toISOString().split('T')[0]
+    if (!date || date > hoy) {
+        alert("Debes informar una fecha menor o igual a hoy")
+        return
+    }
+    
+    peticion_alta.open("POST", "http://localhost:5000/api/v1.0/new", true)
+    peticion_alta.onload = peticion_alta_handler
+    peticion_alta.onerror = function() {alert("No se ha podido completar la petición de movimientos")} //Si todo falla, ejectura esta función
+    peticion_alta.setRequestHeader("Content-Type", "application/json") //Cabecera --- Se interpreta lo que llega como un JSON
+    const data_json = JSON.stringify({date: date, concept: concept, quantity: quantity}) //Convierte los datos en un json
+    peticion_alta.send(data_json) //Envía el JSON
+}
 
 window.onload = function() {
 peticion_todos.open("GET", "http://localhost:5000/api/v1.0/all", true) //Programación asíncrona indicando true --- Como si escribieras la url
 peticion_todos.onload = peticion_todos_handler //Cuando estés cargada, te procesa handler -- Es un evento
 peticion_todos.onerror = function() {alert("No se ha podido completar la petición de movimientos")} //Si todo falla, ejectura esta función
 peticion_todos.send() //Si todo está correcto, lo envía/ejecuta
+
+document.querySelector("#btn_crear").addEventListener("click", //Crear botón "Nuevo"
+    function(ev) {
+        ev.preventDefault() //Captura evento y predomina javascript sobre HTML
+        document.querySelector("#movement_detail").classList.remove("inactive") //Hacer visible el botón de "Nuevo" quitándole la clase que tiene
+        }
+    )
+
+document.querySelector("#btn_cerrar").onclick = function(ev) {
+    ev.preventDefault()
+    document.querySelector("#movement_detail").classList.add("inactive") //Hacer invisible el botón de "Nuevo" quitándole la clase que tiene
+    }
+
+document.querySelector("#btn_aceptar").onclick = altaMovimiento //El evento aceptar viene definida en la función
 }
